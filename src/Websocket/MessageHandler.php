8418eda25fa8f8c35bd5e6cbd32c24d4c7ffe92c
Maxim Gabidullin <after@ya.ru>
2023-01-04 17:34:15 +0300

    session-manager

--- src/Websocket/MessageHandler.php
+++ src/Websocket/MessageHandler.php
@@ -31,7 +31,7 @@ class MessageHandler implements MessageComponentInterface
         $this->output->writeln('New connection');
     }
 
-    public function onMessage(ConnectionInterface $from, $msg)
+    public function onMessage(ConnectionInterface $connection, $msg)
     {
         try {
             $message = json_decode($msg);
@@ -39,103 +39,19 @@ class MessageHandler implements MessageComponentInterface
 
             switch ($message->command) {
                 case 'add_op_message':
-                    $this->operatorConnections->add($from);
-                    $message = json_decode($msg);
-                    $session = $this->chatManager->getSession($message->session);
-                    $message->session = $session;
-                    $this->chatManager->addMessage($session, $message);
-                    $this->operatorConnections->send($msg);
-                    if (array_key_exists($session->getName(), $this->sessions)) {
-                        $this->sessions[$session->getName()]->send($msg);
-                    }
+                    $this->operatorAddMessage($connection, $msg);
                     break;
                 case 'get_sessions':
-                    $this->operatorConnections->add($from);
-
-                    $sessions = $this->operatorManager->getSessions();
-                    $this->output->writeln('OPERATOR Found sessions: ' . count($sessions));
-                    foreach ($sessions as $session) {
-                        $msg = (object)[
-                            'type' => 'session',
-                            'session' => (object)[
-                                'name' => $session['session'],
-                                'id' => $session['id'],
-                                'started' => $session['last_message'],
-                            ],
-                        ];
-                        $msg = json_encode($msg, JSON_FORCE_OBJECT);
-                        $this->operatorConnections->send($msg);
-                    }
+                    $this->operatorGetSessions($connection, $msg);
                     break;
                 case 'get_history':
-                    $session = $this->chatManager->getSession($message->session);
-                    $this->sessions[$session->getName()] = $from;
-                    $chats = $this->chatManager->getChats($session);
-                    $this->output->writeln('Found messages: ' . count($chats));
-                    if (count($chats)) {
-                        foreach ($chats as $chat) {
-                            $message = new Message(
-                                name: $chat->getName(),
-                                message: $chat->getMessage(),
-                                session: (string)$chat->getSession(),
-                                isOperator: $chat->isIsOperator(),
-                            );
-                            $msg = json_encode($message);
-                            $from->send($msg);
-                        }
-                    } else {
-                        $answer = new Message(
-                            name: 'Чат-бот',
-                            message: 'Здравствуйте!',
-                            session: $message->session,
-                            isOperator: true,
-                        );
-                        $this->chatManager->addMessage($session, $answer);
-                        $msg = json_encode($answer);
-                        $from->send($msg);
-                    }
+                    $this->getHistory($message, $connection, $msg);
                     break;
                 case 'get_op_history':
-                    $session = $this->chatManager->getSession($message->session);
-                    $chats = $this->chatManager->getChats($session);
-                    $this->output->writeln('Found messages: ' . count($chats));
-                    if (count($chats)) {
-                        foreach ($chats as $chat) {
-                            $message = new Message(
-                                name: $chat->getName(),
-                                message: $chat->getMessage(),
-                                session: (string)$chat->getSession(),
-                                isOperator: $chat->isIsOperator(),
-                            );
-                            $msg = json_encode($message);
-                            $from->send($msg);
-                        }
-                    } else {
-                        $answer = new Message(
-                            name: 'Чат-бот',
-                            message: 'Здравствуйте!',
-                            session: $message->session,
-                            isOperator: true,
-                        );
-                        $this->chatManager->addMessage($session, $answer);
-                        $msg = json_encode($answer);
-                        $this->operatorConnections->send($msg);
-                    }
+                    $this->operatorGetHistory($message, $connection, $msg);
                     break;
                 case 'add_message':
-                    $session = $this->chatManager->getSession($message->session);
-                    $this->sessions[$session->getName()] = $from;
-
-                    $message = json_decode($msg);
-                    $message->isOperator = false;
-                    $message->session = $session;
-                    $this->chatManager->addMessage($session, $message);
-                    $from->send($msg);
-                    if (!is_null($this->operator)) {
-                        if (!$this->operator->send($msg)) {
-                            $this->operator = null;
-                        }
-                    }
+                    $this->addMessage($message, $connection, $msg);
             }
         } catch (\Exception $exception) {
             $this->output->writeln($exception->getMessage());
@@ -152,5 +68,139 @@ class MessageHandler implements MessageComponentInterface
     {
         $this->operatorConnections->del($conn);
         $conn->close();
+    }
+
+    /**
+     * @param ConnectionInterface $connection
+     * @param string $msg
+     * @return void
+     */
+    public function operatorAddMessage(ConnectionInterface $connection, string $msg): void
+    {
+        $this->operatorConnections->add($connection);
+        $message = json_decode($msg);
+        $session = $this->chatManager->getSession($message->session);
+        $message->session = $session;
+        $this->chatManager->addMessage($session, $message);
+        $this->operatorConnections->send($msg);
+        if (array_key_exists($session->getName(), $this->sessions)) {
+            $this->sessions[$session->getName()]->send($msg);
+        }
+    }
+
+    /**
+     * @param ConnectionInterface $connection
+     * @param object $msg
+     * @return void
+     * @throws \Doctrine\DBAL\Exception
+     */
+    public function operatorGetSessions(ConnectionInterface $connection, object $msg): void
+    {
+        $this->operatorConnections->add($connection);
+
+        $sessions = $this->operatorManager->getSessions();
+        $this->output->writeln('OPERATOR Found sessions: ' . count($sessions));
+        foreach ($sessions as $session) {
+            $msg = (object)[
+                'type' => 'session',
+                'session' => (object)[
+                    'name' => $session['session'],
+                    'id' => $session['id'],
+                    'started' => $session['last_message'],
+                ],
+            ];
+            $msg = json_encode($msg, JSON_FORCE_OBJECT);
+            $this->operatorConnections->send($msg);
+        }
+    }
+
+    /**
+     * @param mixed $message
+     * @param ConnectionInterface $connection
+     * @param false|string $msg
+     * @return void
+     */
+    public function getHistory(mixed $message, ConnectionInterface $connection, false|string $msg): void
+    {
+        $session = $this->chatManager->getSession($message->session);
+        $this->sessions[$session->getName()] = $connection;
+        $chats = $this->chatManager->getChats($session);
+        $this->output->writeln('Found messages: ' . count($chats));
+        if (count($chats)) {
+            foreach ($chats as $chat) {
+                $message = new Message(
+                    name: $chat->getName(),
+                    message: $chat->getMessage(),
+                    session: (string)$chat->getSession(),
+                    isOperator: $chat->isIsOperator(),
+                );
+                $msg = json_encode($message);
+                $connection->send($msg);
+            }
+        } else {
+            $answer = new Message(
+                name: 'Чат-бот',
+                message: 'Здравствуйте!',
+                session: $message->session,
+                isOperator: true,
+            );
+            $this->chatManager->addMessage($session, $answer);
+            $msg = json_encode($answer);
+            $connection->send($msg);
+        }
+    }
+
+    /**
+     * @param mixed $message
+     * @param ConnectionInterface $connection
+     * @param false|string $msg
+     * @return void
+     */
+    public function operatorGetHistory(mixed $message, ConnectionInterface $connection, false|string $msg): void
+    {
+        $session = $this->chatManager->getSession($message->session);
+        $chats = $this->chatManager->getChats($session);
+        $this->output->writeln('Found messages: ' . count($chats));
+        if (count($chats)) {
+            foreach ($chats as $chat) {
+                $message = new Message(
+                    name: $chat->getName(),
+                    message: $chat->getMessage(),
+                    session: (string)$chat->getSession(),
+                    isOperator: $chat->isIsOperator(),
+                );
+                $msg = json_encode($message);
+                $connection->send($msg);
+            }
+        } else {
+            $answer = new Message(
+                name: 'Чат-бот',
+                message: 'Здравствуйте!',
+                session: $message->session,
+                isOperator: true,
+            );
+            $this->chatManager->addMessage($session, $answer);
+            $msg = json_encode($answer);
+            $this->operatorConnections->send($msg);
+        }
+    }
+
+    /**
+     * @param mixed $message
+     * @param ConnectionInterface $connection
+     * @param string $msg
+     * @return void
+     */
+    public function addMessage(mixed $message, ConnectionInterface $connection, string $msg): void
+    {
+        $session = $this->chatManager->getSession($message->session);
+        $this->sessions[$session->getName()] = $connection;
+
+        $message = json_decode($msg);
+        $message->isOperator = false;
+        $message->session = $session;
+        $this->chatManager->addMessage($session, $message);
+        $connection->send($msg);
+        $this->operatorConnections->send($msg);
     }
 }
--- src/Websocket/SessionManager.php
+++ src/Websocket/SessionManager.php
@@ -2,10 +2,50 @@
 
 namespace App\Websocket;
 
+use Ratchet\ConnectionInterface;
+
 class SessionManager
 {
     /**
      * @var ConnectionManager[]
      */
     private array $sessions = [];
+
+    /**
+     * @param string $session
+     * @param ConnectionInterface $connection
+     * @return void
+     */
+    public function add(string $session, ConnectionInterface $connection): void
+    {
+        if (!array_key_exists($session, $this->sessions)) {
+            $this->sessions[$session] = new ConnectionManager();
+        }
+        $this->sessions[$session]->add($connection);
+    }
+
+    /**
+     * @param ConnectionInterface $connection
+     * @return void
+     */
+    public function del(ConnectionInterface $connection): void
+    {
+        foreach ($this->sessions as $session => $connectionManager) {
+            $connectionManager->del($connection);
+        }
+    }
+
+    /**
+     * @param string $session
+     * @param string $message
+     * @return void
+     * @throws \Exception
+     */
+    public function send(string $session, string $message): void
+    {
+        if (!array_key_exists($session, $this->sessions)) {
+            throw new \Exception("Session '$session' not found");
+        }
+        $this->sessions[$session]->send($message);
+    }
 }
